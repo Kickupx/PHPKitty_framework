e4fcb10a47a59e11253c40c289cb124f0f738af6
Kickupx <Kickupx@gmail.com>
2017-01-16 13:23:08 +0100

    Added basic module classes

---
+++ src/PHPKitty/IModule.php
@@ -0,0 +1,6 @@
+<?php
+namespace PHPKitty;
+
+interface IModule {
+    public function process(array $input);
+}
---
+++ src/PHPKitty/Module.php
@@ -0,0 +1,8 @@
+<?php
+namespace PHPKitty;
+
+class Module implements IModule {
+    public function process(array $input) {
+        return null;
+    }
+}
