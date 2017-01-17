8c5208360413c65e549d35dae55d42dc162e8567
Kickupx <Kickupx@gmail.com>
2017-01-18 00:34:36 +0100

    Fixed stuff

--- src/FileTemplateLoader.php
+++ src/FileTemplateLoader.php
@@ -3,7 +3,7 @@ namespace PHPKitty;
 
 class FileTemplateLoader extends \Twig_Loader_Filesystem {
     public function getSourceContext($name) {
-        $source = parent::getSourceCode($name);
+        $source = parent::getSourceContext($name);
         $code = '{% botkitty_lazy_module_emit %}' . $source->getCode();
         return new \Twig_Source($code, $source->getName(), $source->getPath()); 
     }
