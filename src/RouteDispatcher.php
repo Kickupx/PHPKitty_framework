2c5101174b7ad2751dc8881e799b8ed48acec63e
Kickupx <Kickupx@gmail.com>
2017-01-17 12:38:21 +0100

    Made it possible to specify twig factory on route dispatcher construction

--- src/RouteDispatcher.php
+++ src/RouteDispatcher.php
@@ -5,10 +5,12 @@ class RouteDispatcher {
     private $router;
     private $user_permissions;
     private $module_list;
+    private $twig_factory;
 
-    public function __construct(UserPermissions $user_permissions, array $module_list) {
+    public function __construct(UserPermissions $user_permissions, array $module_list, $twig_factory = null) {
         $this->user_permissions = $user_permissions;
         $this->module_list = $module_list;
+        $this->twig_factory = $twig_factory;
         $this->router = new \FastRoute\RouteCollector(
             new \FastRoute\RouteParser\Std(), new \FastRoute\DataGenerator\GroupCountBased() 
         );
@@ -16,7 +18,7 @@ class RouteDispatcher {
 
     public function render($modules_call_decl, ITemplate $template, UserPermissions $user_permissions) {
         $variables = Module::processModules($modules_call_decl, $this->module_list);
-        $twig = $template->makeTwig($user_permissions);
+        $twig = $template->makeTwig($user_permissions, $twig_factory);
         return $twig->render($template->name(), $variables);
     }
 
