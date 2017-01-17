1f3ac44bdb4f88e2dc32321f699d57e6d5dc7337
Kickupx <Kickupx@gmail.com>
2017-01-17 19:08:47 +0100

    Made ArrayWrapper more extendable

--- src/ArrayWrapper.php
+++ src/ArrayWrapper.php
@@ -1,7 +1,7 @@
 <?php
 namespace PHPKitty;
 
-class ArrayWrapper {
+class ArrayWrapper implements \ArrayAccess {
     private $array;
 
     public function __construct(array $array) {
@@ -9,8 +9,32 @@ class ArrayWrapper {
     }
 
     public function __get($name) {
+        return $this->get_impl($name);
+    }
+
+    public function offsetExists ($offset) {
+        return array_key_exists($offset, $this->array);
+    }
+
+    public function offsetGet ($offset) {
+        return $this->get_impl($offset);
+    }
+
+    public function offsetSet ($offset, $value) {
+        self::invalid_op($offset);
+    }
+
+    public function offsetUnset ($offset) {
+        self::invalid_op($offset);
+    }
+
+    protected function get_impl($name) {
         if(!array_key_exists($name, $this->array))
             return null;
         return $this->array[$name];
+    }
+
+    private static function invalid_op($offset) {
+        throw new Exception("Can not set index $offset. ArrayWrapper is const.");
     }
 }
--- tests/ArrayWrapperTest.php
+++ tests/ArrayWrapperTest.php
@@ -15,4 +15,16 @@ class ArrayWrapperTest extends TestCase {
         $w = new ArrayWrapper(['foo' => 42]);
         $this->assertEquals($w->foo, 42);
     }
+
+    function testArrayLike() {
+        $w = new ArrayWrapper(['foo' => 42]);
+        $this->assertTrue(isset($w['foo']));
+        $this->assertEquals($w['foo'], 42);
+    }
+
+    function testConst() {
+        $this->expectException(PHPKitty\Exception::class);
+        $w = new ArrayWrapper([]);
+        $w['bar'] = 42;
+    }
 }
