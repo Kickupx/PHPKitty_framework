784cb0a64262d16357736ab5a7860c0c8aeed13e
Kickupx <Kickupx@gmail.com>
2017-01-17 10:15:57 +0100

    Added FileSystemTemplate

---
+++ src/Template/FileSystemTemplate.php
@@ -0,0 +1,27 @@
+<?php
+namespace PHPKitty\Template;
+
+use PHPKitty\ITemplate;
+use PHPKitty\UserPermissions;
+
+class FileSystemTemplate implements ITemplate {
+    private $dir;
+    private $name_v;
+
+    public function __construct($dir, $name) {
+        $this->dir = $dir;
+        $this->name_v = $name;
+    }
+
+    public function name() {
+        return $name_v;
+    }
+
+    public function makeTwig(UserPermissions $user_permissions) {
+        $twig_loader = new \Twig_Loader_Filesystem($this->dir);
+        $twig = new \Twig_Environment($twig_loader);
+
+        $twig->addTokenParser(new \PHPKitty\TwigTokenParser\Permission($user_permissions));
+        return $twig;
+    }
+}
