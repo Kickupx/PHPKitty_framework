f19e9ed4a8014ff9f8ec9391547b7ed2fe3b1bb9
Kickupx <Kickupx@gmail.com>
2017-01-18 01:05:25 +0100

    Added has check for lazy module processor store

--- src/LazyModuleProcessorStore.php
+++ src/LazyModuleProcessorStore.php
@@ -8,6 +8,10 @@ class LazyModuleProcessorStore {
         $this->lazy_modules[$output_key] = $module;
     }
 
+    public function has($output_key) {
+        return array_key_exists($output_key, $this->lazy_modules);
+    }
+
     public function get($output_key) {
         if(!isset($this->lazy_modules[$output_key]))
             throw new Exception("Module which outputs in key $output_key is not defined");
